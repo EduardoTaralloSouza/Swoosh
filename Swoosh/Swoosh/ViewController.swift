db501f0afd697ca4b2234d56bb1a271e947bcad5
Eduardo Tarallo Souza <eduardotarallo85@gmail.com>
2018-11-14 16:12:04 -0200

    Segues and unwind

--- Swoosh/Swoosh/ViewController.swift
+++ Swoosh/Swoosh/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
 
 
 }
