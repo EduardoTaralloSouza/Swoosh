908acbde2051ebb0f74a889c2fd769cb64e49573
Eduardo Tarallo Souza <eduardotarallo85@gmail.com>
2018-11-17 10:50:00 -0200

    Create a template for players and linking actions

--- Swoosh/Swoosh/Controller/LeagueVC.swift
+++ Swoosh/Swoosh/Controller/LeagueVC.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var nextBtn: BorderButton!
+    
+    
+    // MARK: - Variables
+    var player: Player!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        player = Player()
     }
     
+    
+    // MARK: - Actions
+    @IBAction func onMensTapped(_ sender: Any) {
+       selectLeague(leagueType: "mans")
+    }
+    
+    @IBAction func onWomansTapped(_ sender: Any) {
+        selectLeague(leagueType: "womans")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
-
+    
+    // Methods
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
+    
+    
 }
---
+++ Swoosh/Swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Eduardo Tarallo on 16/11/18.
+//  Copyright © 2018 Eduardo Tarallo. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var sectedSkillLevel: String?
+}
